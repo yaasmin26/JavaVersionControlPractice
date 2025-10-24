e443446f9af2d66a07891ca547c6fad9152d9ca8
yaasmin26 <yaasminramadhani@gmail.com>
2025-10-24 14:43:32 +0700

    update(readme): move folder to root

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Git from IntelliJ!");
+        System.out.println("Haiii Yaasmin");
+        System.out.println("INI UPDATE BARU");
     }
 }
